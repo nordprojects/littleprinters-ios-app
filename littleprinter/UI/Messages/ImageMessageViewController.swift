de9a4324b9cb66eee20d37dc024fd03faa2e945f
Michael Colville <mike.colville@icloud.com>
2018-08-10 18:19:14 +0100

    Add image selection to image message

--- littleprinter/UI/Messages/ImageMessageViewController.swift
+++ littleprinter/UI/Messages/ImageMessageViewController.swift
@@ -8,59 +8,51 @@
 
 import UIKit
 
+protocol PhotoPickerDelegate {
+    func pickerDidReturn(_ image: UIImage)
+}
+
 class ImageMessageViewController: UIViewController {
     
     var recipient: Printer?
     
-    lazy var imageView = UIImageView()
-    lazy var sendButton = UIButton()
+    lazy var imageTemplateView: ImageTemplateView = {
+        let view = ImageTemplateView()
+        return view
+    }()
     
+    lazy var receiptPreviewView: ReceiptPreviewView = {
+        let view = ReceiptPreviewView(innerView: imageTemplateView)
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "receipt-background")!)
         
-        let label = UILabel()
-        label.text = "Select Image"
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        view.addSubview(label)
-        label.snp.makeConstraints { (make) in
-            make.top.left.equalTo(view.safeAreaLayoutGuide).offset(20)
+        imageTemplateView.delegate = self
+        
+        view.addSubview(receiptPreviewView)
+        
+        receiptPreviewView.snp.makeConstraints { (make) in
+            make.top.equalTo(view)
+            make.left.right.equalTo(view).inset(24)
         }
         
-        imageView.layer.borderWidth = 1
-        imageView.layer.borderColor = UIColor.black.cgColor
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = #imageLiteral(resourceName: "a7bA03H")
-        view.addSubview(imageView)
-        imageView.snp.makeConstraints { (make) in
-            make.top.equalTo(label.snp.bottom).offset(10)
-            make.width.equalTo(view).offset(-40)
-            make.height.equalTo(imageView.snp.width)
-            make.centerX.equalTo(view)
-        }
-        
-        sendButton.addTarget(self, action: #selector(sendPressed), for: .touchUpInside)
-        sendButton.setTitle("Send", for: .normal)
-        sendButton.setTitleColor(.black, for: .normal)
-        sendButton.layer.borderColor = UIColor.black.cgColor
-        sendButton.layer.borderWidth = 1
-        view.addSubview(sendButton)
-        sendButton.snp.makeConstraints { (make) in
-            make.top.equalTo(imageView.snp.bottom).offset(10)
-            make.right.equalTo(imageView)
-            make.width.equalTo(120)
-            make.height.equalTo(40)
-        }
         if let printer = recipient {
             self.title = printer.info.name
         }
+    }
+    
+    @objc func choosePressed() {
+        PhotoPicker.shared.startMediaBrowserFromController(self)
     }
     
     @objc func sendPressed() {
         let fromName = User.shared.name ?? "App"
 
         if let printer = recipient,
-            let image = imageView.image {
+            let image = imageTemplateView.imageView.image {
             do {
                 let message = try SiriusMessage(image: image, to: printer.key, from: fromName)
                 navigationController?.pushViewController(
@@ -74,3 +66,114 @@ class ImageMessageViewController: UIViewController {
         }
     }
 }
+
+extension ImageMessageViewController: PhotoPickerDelegate {
+    func pickerDidReturn(_ image: UIImage) {
+        imageTemplateView.imageView.image = image
+        imageTemplateView.hideChooseButton()
+    }
+}
+
+class ImageTemplateView: UIView, UITextViewDelegate {
+    
+    lazy var fuzzImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "fuzz")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.text = "tap to add caption"
+        textView.textAlignment = .center
+        textView.font = UIFont(name: "Permanent Marker", size: 22)
+        textView.isScrollEnabled = false
+        textView.delegate = self
+        textView.textColor = UIColor(white: 0.0, alpha: 0.15)
+        return textView
+    }()
+    
+    lazy var chooseButton: ChunkyButton = {
+        let button = ChunkyButton()
+        button.topColor = UIColor(hex: 0x89BEFE)
+        button.borderColor = UIColor(hex: 0x89BEFE)
+        button.shadowColor = UIColor(hex: 0x4177AF)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Black", size: 14)
+        button.setTitle("Choose image", for: .normal)
+        return button
+    }()
+    
+    var delegate: ImageMessageViewController?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(fuzzImageView)
+        addSubview(imageView)
+        addSubview(textView)
+        addSubview(chooseButton)
+        
+        fuzzImageView.snp.makeConstraints { (make) in
+            make.left.right.top.equalToSuperview()
+            make.height.equalTo(fuzzImageView.snp.width).multipliedBy(0.75)
+        }
+        
+        imageView.snp.makeConstraints { (make) in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(fuzzImageView)
+        }
+        
+        textView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.top.equalTo(fuzzImageView.snp.bottom).offset(6)
+            make.height.greaterThanOrEqualTo(31)
+        }
+        
+        chooseButton.snp.makeConstraints { (make) in
+            make.center.equalTo(fuzzImageView)
+            make.width.equalTo(143)
+            make.height.equalTo(44)
+        }
+        
+        chooseButton.addTarget(self, action: #selector(choosePressed), for: .touchUpInside)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func choosePressed() {
+        delegate?.choosePressed()
+    }
+    
+    // Public methods
+    func hideChooseButton() {
+        chooseButton.isHidden = true
+    }
+    
+    // Hack in placeholder
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor(white: 0.0, alpha: 0.15) {
+            textView.text = nil
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "tap to add caption"
+            textView.textColor = UIColor(white: 0.0, alpha: 0.15)
+        }
+    }
+}
+
---
+++ littleprinter/UI/Messages/PhotoPicker.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoPicker.swift
+//  inspiral
+//
+//  Created by Michael Colville on 03/08/2018.
+//
+
+import UIKit
+
+class PhotoPicker: NSObject {
+    
+    var controller: (UIViewController & PhotoPickerDelegate)?
+    var mediaUI: UIImagePickerController?
+    static let shared = PhotoPicker()
+
+    func startMediaBrowserFromController(_ controller: (UIViewController & PhotoPickerDelegate)) {
+        self.controller = controller
+        
+        mediaUI = UIImagePickerController()
+        mediaUI?.sourceType = .photoLibrary
+        mediaUI?.allowsEditing = false
+        mediaUI?.delegate = self
+
+        controller.present(mediaUI!, animated: true, completion: nil)
+    }
+    
+    func exitPressed() {
+        controller?.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PhotoPicker: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            controller?.pickerDidReturn(image)
+        }
+        controller?.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PhotoPicker: UINavigationControllerDelegate {
+    
+}
